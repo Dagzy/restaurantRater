1bfe0ea9f412d31d976345e3200a9421721e1edc
Dagzy <quincye.williams@gmail.com>
2017-11-06 15:04:42 -0500

    QW added create controller and view

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using RestaurantRater.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext db = new RestaurantDBContext();
         // GET: Restaurant
         public ActionResult Index()
         {
+            return View(db.Restaurants.ToList());
+        }
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID, Name")]Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
